2641cd0d344819fe4772a175e00954cf61ceea92
Eddie McConkie <edwardmcconkie@gmail.com>
2023-03-29 16:41:14 -0600

    Maybe optimize Three

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { threeMinifier } from '@yushijinhun/three-minifier-rollup';
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
+	plugins: [{ ...threeMinifier(), enforce: 'pre' }, sveltekit()],
 	ssr: {
 		noExternal: ['three'],
 	},
